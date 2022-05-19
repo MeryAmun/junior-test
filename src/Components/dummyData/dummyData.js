a55840bfced979fbe8cfea0372fa26821738fae4
MeryAmun <chebesiawah@gmail.com>
2022-05-19 13:43:13 +0100

    'data'

---
+++ src/Components/dummyData/dummyData.js
@@ -0,0 +1,18 @@
+import { productA, productB, productC, productD} from '../../assets/index'
+
+const data = [
+    {
+        img:productA, productName:"Apollo Running Short", pricr: '$50.00'
+    },
+    {
+        img:productB, productName:"Apollo Running Short", pricr: '$50.00'
+    },
+    {
+        img:productC, productName:"Apollo Running Short", pricr: '$50.00'
+    },
+    {
+        img:productD, productName:"Apollo Running Short", pricr: '$50.00'
+    }
+]
+
+export default data
---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import productA from './images/Product A.png';
+import productB from './images/Product B.png';
+import productC from './images/Product C.png';
+import productD from './images/Product D.png';
+
+
+
+
+export {
+    productA,productB,productC,productD
+}

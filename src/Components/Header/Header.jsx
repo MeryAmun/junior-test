79def8e4d96d9dadc72a9114bfba54ffad17b637
MeryAmun <chebesiawah@gmail.com>
2022-05-21 03:41:58 +0100

    'correction'

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {BsCart,BsArrowCounterclockwise} from '../../Components/Icons/index'
+import prodctData from '../dummyData/dummyData'
 import './header.css'
 
 export default class Header extends Component {
@@ -9,19 +10,19 @@ export default class Header extends Component {
       <div className='header__wrapper'>
           <div className="header__firstchild">
               <div className="child__wrapper">
-              <span className="women">
+              <span className="gender">
                   women
               </span>
               <span className="border"></span>
               </div>
               <div className="child__wrapper">
-              <span className="men">
+              <span className="gender">
                   men
               </span>
               <span className="border"></span>
               </div>
               <div className="child__wrapper">
-              <span className="children">
+              <span className="gender">
                   kids
               </span>
               <span className="border"></span>
@@ -37,7 +38,10 @@ export default class Header extends Component {
           </div>
           <div className="header__currency">
             <select>
-              <option value="USD">$</option>
+            <option value="">currency</option>
+              <option value="USD">$ USD</option>
+              <option value="EUR">€ EUR</option>
+              <option value="JPN">¥ JPN</option>
             </select>
           <span className="cart">
             <BsCart/>
--- src/Components/dummyData/dummyData.js
+++ src/Components/dummyData/dummyData.js
@@ -1,18 +1,56 @@
 import { productA, productB, productC, productD} from '../../assets/index'
-
 const data = [
-    {
-        id:'1', image:productA, productName:"Apollo Running Short", productPrice: '$50.00'
-    },
-    {
-        id:'2', image:productB, productName:"Apollo Running Short", productPrice: '$50.00'
-    },
-    {
-        id:'3', image:productC, productName:"Apollo Running Short", productPrice: '$50.00'
-    },
-    {
-        id:'4', image:productD, productName:"Apollo Running Short", productPrice: '$50.00'
-    }
+{
+    gender: 'WOMEN',
+    products:[
+        {
+            id:'1', image:productA, productName:"Apollo Running Short", productPrice: '$50.00'
+        },
+        {
+            id:'2', image:productB, productName:"Apollo Running Short", productPrice: '$50.00'
+        },
+        {
+            id:'3', image:productC, productName:"Apollo Running Short", productPrice: '$50.00'
+        },
+        {
+            id:'4', image:productD, productName:"Apollo Running Short", productPrice: '$50.00'
+        }
+    ]
+},
+{
+    gender: 'MEN',
+    products:[
+        {
+            id:'5', image:productA, productName:"Apollo Running Short", productPrice: '$50.00'
+        },
+        {
+            id:'6', image:productB, productName:"Apollo Running Short", productPrice: '$50.00'
+        },
+        {
+            id:'7', image:productC, productName:"Apollo Running Short", productPrice: '$50.00'
+        },
+        {
+            id:'8', image:productD, productName:"Apollo Running Short", productPrice: '$50.00'
+        }
+    ]
+},
+{
+    gender: 'kIDs',
+    products:[
+        {
+            id:'9', image:productA, productName:"Apollo Running Short", productPrice: '$50.00'
+        },
+        {
+            id:'10', image:productB, productName:"Apollo Running Short", productPrice: '$50.00'
+        },
+        {
+            id:'11', image:productC, productName:"Apollo Running Short", productPrice: '$50.00'
+        },
+        {
+            id:'12', image:productD, productName:"Apollo Running Short", productPrice: '$50.00'
+        }
+    ]
+}
 ]
 
 export default data

4bde6deb2e4687267a1ffa62450006ab8a18ea78
MeryAmun <chebesiawah@gmail.com>
2022-05-23 22:38:54 +0100

    'pdp'

---
+++ src/Components/ProductDetails/ProductDetailsPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './productDetails.css'
+
+const ProductDetailsPage = () => {
+  return (
+    <div>ProductDetailsPage</div>
+  )
+}
+
+export default ProductDetailsPage

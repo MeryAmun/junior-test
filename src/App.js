c138f81c4992401c2c143875d88eb55739eca7ca
MeryAmun <chebesiawah@gmail.com>
2022-05-25 01:10:16 +0100

    'overlay'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import Category from './Components/Category/Category';
+import Cart from './Components/Cart/Cart';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       {/* <Category/> */}
+      <Routes>
+        <Route path='/cart' element={<Cart/>}/>
+      </Routes>
     </div>
   );
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,5 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal'
 import { BsCart, BsArrowCounterclockwise } from '../../Components/Icons/index'
 import productData from '../dummyData/dummyData';
 import CategoryCard from '../../Cards/CategoryCard/CategoryCard';
@@ -7,40 +8,41 @@ import './header.css'
 import Category from '../Category/Category';
 
 const Header = () => {
-const [fetchedProducts, setFetchedProducts] = useState(productData);
-const [open, setOpen] = useState(false);
-const [value, setValue] = useState(0);
+  const [fetchedProducts, setFetchedProducts] = useState(productData);
+  const [cartIsopen, setCartIsOpen] = useState(false);
+  const [value, setValue] = useState(0);
 
 
-const reload = () => {
-  window.location.reload()
-}
+  const reload = () => {
+    window.location.reload()
+  }
 
-const { products , category} = fetchedProducts[value]
+ 
+  const { products, category } = fetchedProducts[value]
 
-    return (
-      <div className="cart__body_wrapper">
-        <div className='header__wrapper'>
+  return (
+    <div className="cart__body_wrapper">
+      <div className='header__wrapper'>
         <div className="header__firstchild">
           {
             productData.map((data, index) => (
               <div className="child__wrapper" key={index}>
 
                 <button key={index}
-                onClick={() => setValue(index)}
-                className={`gender ${index === value && 'border'}`}
+                  onClick={() => setValue(index)}
+                  className={`gender ${index === value && 'border'}`}
                 >
                   {data.category}
                 </button>
                 <span className=""></span>
               </div>
             ))
-            }
+          }
         </div>
         <div className="header__logo__wrapper">
           <div className="logo__container">
             <span className="">
-              <BsArrowCounterclockwise className='logo' onClick={reload}/>
+              <BsArrowCounterclockwise className='logo' onClick={reload} />
             </span>
             <span className="arrow"></span>
           </div>
@@ -53,35 +55,55 @@ const { products , category} = fetchedProducts[value]
             <option value="JPN">¥ JPN</option>
           </select>
           <span className="cart">
-            <BsCart onClick={() => setOpen(!open)}/>
+            <BsCart onClick={() => setCartIsOpen(true)} />
           </span>
         </div>
-        {/* <CartItemCard className='cart__overlay'/> */}
+        <div className="cart__overlay_wrapper">
+        <Modal 
+        isOpen={cartIsopen} 
+        style={customStyles}
+       >
+        <CartItemCard/>
+        <button className="btn" 
+         onClick={() => setCartIsOpen(false)}>close</button>
+        </Modal>
+        </div>
       </div>
       <div className='category__wrapper'>
-      
-       <div className='category__header'>
-         <h2 className='category__header-text'>
-   {category}
-         </h2>
-       </div>
-        
-       <div className='category__items-wrapper'>
-        {
-          products.map((data) => (
-            <CategoryCard
-            key={data.id}
-            image={data.image}
-            productName={data.productName}
-            price={data.productPrice}/>
-             
-          ))
-        }
-        </div>
-       
-      </div>
-      </div>
-    )
-  }
-  export default Header
 
+        <div className='category__header'>
+          <h2 className='category__header-text'>
+            {category}
+          </h2>
+        </div>
+
+        <div className='category__items-wrapper'>
+          {
+            products.map((data) => (
+              <CategoryCard
+                key={data.id}
+                image={data.image}
+                productName={data.productName}
+                price={data.productPrice} />
+
+            ))
+          }
+        </div>
+
+      </div>
+    </div>
+  )
+}
+export default Header
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    
+  },
+};

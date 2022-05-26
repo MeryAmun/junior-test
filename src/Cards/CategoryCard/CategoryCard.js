b338fc10941105d9ff089cbbb2788095a7179ed7
MeryAmun <chebesiawah@gmail.com>
2022-05-26 03:52:57 +0100

    'cart'

--- src/Cards/CategoryCard/CategoryCard.js
+++ src/Cards/CategoryCard/CategoryCard.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import './categoryCard.css'
+import { BsCart } from '../../Components/Icons'
 
 
  const CategoryCard = ({ id, image, productName, price}) => {
   
     return (
       <div className='category__item' key={id}>
-      <img src={image} alt='product' className='product__image'/>
+    <div className='category__image_wrapper'>
+    <img src={image} alt='product' className='product__image'/>
+    <span className='catigory__cart_icon'>
+      <BsCart/>
+    </span>
+    </div>
     <div className='category__titles'>
     <h3 className='product__name'>{productName}</h3>
     <span className='product__price'>{price}</span>
